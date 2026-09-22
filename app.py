c8e41f3b739c844d24ed3c64718547dbe955dbdd
toadin <daniel.m.dodin@gmail.com>
2026-09-22 10:22:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-# Pseudo-code:
-# 1. Ask the user to type a sentence.
-# 2. Save that sentence in a variable.
-# 3. Split the sentence into separate words.
-# 4. Count how many words are in the list.
-# 5. Print the result.
 
 
-def count_words(sentence):
+
+""" def count_words(sentence):
     words = sentence.split()
     return len(words)
 
 
-user_sentence = input("I am a writer ")
-word_count = count_words(user_sentence)
-print(f"I am a writer {4} words.")
+x = input("Enter a sentence: ")
+word_count = count_words(x)
+print(f"Your sentence has {word_count} words.") """
 
+
+
+def number(x):
+    if x is (1,3,5,7,9):
+        print ('odd')
+    else:
+        print('even')
+x = input("Enter a number")
 
